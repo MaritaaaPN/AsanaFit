072b77f8e1009eebe27e91d3814b5b1309b61c9d
Marita <maritap225@gmail.com>
2025-03-23 22:22:44 +0700

    [BAB 1] INSTALLASI REACT NATIVE

--- App.tsx
+++ App.tsx
@@ -89,7 +89,7 @@ function App(): React.JSX.Element {
             paddingHorizontal: safePadding,
             paddingBottom: safePadding,
           }}>
-          <Section title="Step One">
+          <Section title="Langkah Satu">
             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
             screen and then come back to see your edits.
           </Section>
